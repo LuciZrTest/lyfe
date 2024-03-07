71c22040e8bb9a56a32536dd7e178f136cc8671c
LuciZR <161382979+LuciZrTest@users.noreply.github.com>
2024-03-07 15:12:49 +0530

    𝙼𝙸𝙰 𝙼𝙳 ⚠︎

--- plugins/ping.js
+++ plugins/ping.js
@@ -9,7 +9,7 @@ bot(
   },
   async (message, match) => {
     const start = new Date().getTime()
-    await message.send('```Ping!```')
+    await message.send('```𝙼𝙸𝙰 𝙼𝙳 ⚠︎```')
     const end = new Date().getTime()
     return await message.send('*Pong!*\n ```' + (end - start) + '``` *ms*')
   }
